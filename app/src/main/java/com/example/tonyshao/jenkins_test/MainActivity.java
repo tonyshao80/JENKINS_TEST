13964de912a63c573db50462c8f74946b97c522a
tonyshao80 <tonyshao80@gmail.com>
2017-05-26 15:11:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tonyshao/jenkins_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tonyshao.jenkins_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
